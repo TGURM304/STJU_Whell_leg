6bd8d56a02161e774c239bf37b0ac2b721bd098d
KLnormal <yuyongyikaf530000@163.com>
2025-11-13 21:55:16 +0800

    matlab solve the formula

--- Matlab/my_lqr_clc/get_lqr_len.m
+++ Matlab/my_lqr_clc/get_lqr_len.m
@@ -70,49 +70,88 @@ ddhlr = ddhb + l_br*sin(theta_lr)*ddtheta_lr + l_br*cos(theta_lr)*dtheta_lr*dthe
 
 %进行小角度近似后
 ddphi = R_w*(-ddtheta_wl+ddtheta_wr)/(2*R_l) - l_l*theta_ll*ddtheta_ll/(2*R_l) + l_r*ddtheta_lr/(2*R_l);
-ddSb = R_w*(ddtheta_wl+ddtheta_wr)/2 + l_l*ddtheta_ll/2 + l_r*ddtheta_lr/2;
-ddhb = -l_l*theta_ll*ddtheta_ll/2 - l_r*theta_lr*ddtheta_lr/2;
 ddhll = ddhb + l_bl*theta_ll*ddtheta_ll;
 ddhlr = ddhb + l_br*theta_lr*ddtheta_lr;
+ddSb = R_w*(ddtheta_wl+ddtheta_wr)/2 + l_l*ddtheta_ll/2 + l_r*ddtheta_lr/2;
+ddhb = -l_l*theta_ll*ddtheta_ll/2 - l_r*theta_lr*ddtheta_lr/2;
 ddslr = R_w*ddtheta_wr + l_wr*ddtheta_lr;
 ddsll = R_w*ddtheta_wl + l_wl*ddtheta_ll;
+%计算后的动力学的中间变量
+%%按照从后往前的顺序替换可以保证所有内容都被替换为各个二阶导的基本方程
+fl = (T_lwl - I_w*ddtheta_wl)/R_w;
+fr = (T_lwr - I_w*ddtheta_wr)/R_w;
+Fwsl = fl - m_w*R_w*ddtheta_wl;
+Fwsr = fr - m_w*R_w*ddtheta_wr;
+Fbsl = Fwsl - m_l*ddsll;
+Fbsr = Fwsr - m_l*ddslr;
+Fbhl = (m_b*ddhb + m_b*g - (m_l*ddhlr - m_l*ddhll))/2;
+Fbhr = (m_b*ddhb + m_b*g + (m_l*ddhlr - m_l*ddhll))/2;
+Fwhl = m_l*ddhll + m_l*g + Fbhl;
+Fwhr = m_l*ddhlr + m_l*g + Fbhr;
+
 %动力学方程十五则
-eq10 = m_w*R_w*ddtheta_wl - fl - Fwsl == 0;
-eq11 = m_w*R_w*ddtheta_wr - fr - Fwsr == 0;
-eq12 = I_w*ddtheta_wl - T_lwl - fl*R_w == 0;
-eq13 = I_w*ddtheta_wr - T_lwr - fr*R_w == 0;
-eq1 = m_l*ddslr - (Fwsr - Fbsr) == 0;
-eq2 = m_l*ddsll - (Fwsl - Fbsl) == 0;
-eq3 = m_l*ddhll -(Fwhl - Fbhl - m_l*g) == 0;
-eq4 = m_l*ddhlr - (Fwhr - Fbhr - m_l*g) == 0;
-eq5 = I_ll*ddtheta_ll - ((Fwhl*l_wl + Fbhl*l_bl)*sin(theta_ll) - (Fwsl*l_bl + Fbsl*l_bl)*cos(theta_ll)-T_lwl+T_bll) == 0;
-eq6 = I_lr*ddtheta_lr - ((Fwhr*l_wr + Fbhr*l_br)*sin(theta_lr) - (Fwsl*l_br + Fbsr*l_br)*cos(theta_lr)-T_lwr+T_blr) == 0;
-eq7 = m_b*ddSb - (Fbsl + Fbsr) == 0;
-eq8 = m_b*ddhb - (Fbhl + Fbhr) == 0;
-eq9 = I_b*ddtheta_b - (-(T_bll + T_blr) - (Fbsl + Fbsr)*l_c*cos(theta_b) + (Fbhl + Fbhr)*l_c*sin(theta_b)) == 0;
-eq14 = I_z*ddphi - ((-fl + fr)*R_l) == 0; 
-eq15 = Fwhl - Fwhr == 0;
-eq16 = S - R_w*(theta_wl + theta_wr)/2 == 0;
+eq1 = m_w*R_w*ddtheta_wl - (fl - Fwsl) == 0;
+eq2 = m_w*R_w*ddtheta_wr - (fr - Fwsr) == 0;
+eq3 = I_w*ddtheta_wl - (T_lwl - fl*R_w) == 0;
+eq4 = I_w*ddtheta_wr - (T_lwr - fr*R_w) == 0;
+eq5 = m_l*ddslr - (Fwsr - Fbsr) == 0;
+eq6 = m_l*ddsll - (Fwsl - Fbsl) == 0;
+eq7 = m_l*ddhll - (Fwhl - Fbhl - m_l*g) == 0;
+eq8 = m_l*ddhlr - (Fwhr - Fbhr - m_l*g) == 0;
+eq9 = I_ll*ddtheta_ll - ((Fwhl*l_wl + Fbhl*l_bl)*sin(theta_ll) - (Fwsl*l_wl + Fbsl*l_bl)*cos(theta_ll)-T_lwl+T_bll) == 0;
+eq10 = I_lr*ddtheta_lr - ((Fwhr*l_wr + Fbhr*l_br)*sin(theta_lr) - (Fwsr*l_wr + Fbsr*l_br)*cos(theta_lr)-T_lwr+T_blr) == 0;
+eq11 = m_b*ddSb - (Fbsl + Fbsr) == 0;
+eq12 = m_b*ddhb - (Fbhl+Fbhr-m_b*g) == 0;
+eq13 = I_b*ddtheta_b - (-(T_bll+T_blr)-(Fbsl+Fbsr)*l_c*cos(theta_b)+(Fbhl+Fbhr)*l_c*sin(theta_b)) == 0;
+eq14 = I_z*ddphi - (-fl+fr)*R_l == 0;
+eq15 = Fwhl-Fwhr == 0;
 
-eqations = [eq1,eq2,eq3,eq4,eq5,eq6,eq7,eq8,eq9,eq10,eq11,eq12,eq13,eq13,eq14,eq15,eq16,ddphi,ddSb,ddhb,ddsll,ddslr,ddhlr,ddhll];
-vars =  [theta_ll, theta_lr, theta_b];
-vars_dot = [dtheta_ll, dtheta_lr, dtheta_b];
-%小角度近似
-for v = vars
-    eqations = subs(eqations, sin(v),v);
-    eqations = subs(eqations, cos(v),1);
+%%建立替换规则的元胞数组，通过管理rules来实现管理替换变量
+rules = {
+    Fwhr,m_l*ddhlr + m_l*g + Fbhr;
+    Fwhl , m_l*ddhll + m_l*g + Fbhl;
+    Fbhr , (m_b*ddhb + m_b*g + (m_l*ddhlr - m_l*ddhll))/2;
+    Fbhl , (m_b*ddhb + m_b*g - (m_l*ddhlr - m_l*ddhll))/2;
+    Fbsr , Fwsr - m_l*ddslr;
+    Fbsl , Fwsl - m_l*ddsll;
+    Fwsr , fr - m_w*R_w*ddtheta_wr;
+    Fwsl , fl - m_w*R_w*ddtheta_wl;
+    fr , (T_lwr - I_w*ddtheta_wr)/R_w;
+    fl , (T_lwl - I_w*ddtheta_wl)/R_w;
+    ddphi , R_w*(-ddtheta_wl+ddtheta_wr)/(2*R_l) - l_l*theta_ll*ddtheta_ll/(2*R_l) + l_r*ddtheta_lr/(2*R_l);
+    ddhll , ddhb + l_bl*theta_ll*ddtheta_ll;
+    ddhlr , ddhb + l_br*theta_lr*ddtheta_lr;
+    ddSb , R_w*(ddtheta_wl+ddtheta_wr)/2 + l_l*ddtheta_ll/2 + l_r*ddtheta_lr/2;
+    ddhb , -l_l*theta_ll*ddtheta_ll/2 - l_r*theta_lr*ddtheta_lr/2;
+    ddslr , R_w*ddtheta_wr + l_wr*ddtheta_lr;
+    ddsll , R_w*ddtheta_wl + l_wl*ddtheta_ll;
+    sin(theta_wr),theta_wr;
+    sin(theta_wl),theta_wl;
+    sin(theta_ll),theta_ll;
+    sin(theta_lr),theta_lr;
+    sin(theta_b),theta_b;
+    cos(theta_wr),1;
+    cos(theta_wl),1;
+    cos(theta_ll),1;
+    cos(theta_lr),1;
+    cos(theta_b),1;
+    dtheta_wr, 0;
+    dtheta_wl, 0;
+    dtheta_ll, 0;
+    dtheta_lr, 0;
+    dtheta_b, 0;
+};
+change = [eq14,eq9,eq10,eq11,eq13];
+var = [ddtheta_wl,ddtheta_wr,ddtheta_ll,ddtheta_lr,ddtheta_b];
+for i = 1:size(rules,1)
+    change = subs(change,rules(i,1),rules(i,2));
 end
-for v = vars_dot
-    eqations = subs(eqations, v,0);
-end
-disp(eqations);
 
-target = [ddtheta_wl, ddtheta_wr, ddtheta_ll, ddtheta_lr, ddtheta_b];
-%查矩阵的秩
-[Aeq, beq] = equationsToMatrix(eqations, target);
-disp(rank(Aeq));
-disp("clc_start")
-temp = solve(eq1,eq2,eq3,eq4,eq5,eq6,eq7,eq8,eq9,eq10,eq11,eq12,eq13,eq13,eq14,eq15,eq16,target);
-disp("clc_end")
-disp(temp);
-disp("end");
+answer = solve(change,var)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%此处输入机器人的实际参数
+sym_2_real = {
+    
+    };
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+toc
