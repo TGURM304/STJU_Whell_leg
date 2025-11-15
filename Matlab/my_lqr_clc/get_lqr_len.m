c79723250b36ee0569225fc77dc195297e668b41
KLnormal <yuyongyikaf530000@163.com>
2025-11-15 20:28:48 +0800

    matlab get wrong eq

--- Matlab/my_lqr_clc/get_lqr_len.m
+++ Matlab/my_lqr_clc/get_lqr_len.m
@@ -98,12 +98,12 @@ eq5 = m_l*ddslr - (Fwsr - Fbsr) == 0;
 eq6 = m_l*ddsll - (Fwsl - Fbsl) == 0;
 eq7 = m_l*ddhll - (Fwhl - Fbhl - m_l*g) == 0;
 eq8 = m_l*ddhlr - (Fwhr - Fbhr - m_l*g) == 0;
-eq9 = I_ll*ddtheta_ll - ((Fwhl*l_wl + Fbhl*l_bl)*sin(theta_ll) - (Fwsl*l_wl + Fbsl*l_bl)*cos(theta_ll)-T_lwl+T_bll) == 0;
-eq10 = I_lr*ddtheta_lr - ((Fwhr*l_wr + Fbhr*l_br)*sin(theta_lr) - (Fwsr*l_wr + Fbsr*l_br)*cos(theta_lr)-T_lwr+T_blr) == 0;
+eq9 = I_ll*ddtheta_ll == (Fwhl*l_wl + Fbhl*l_bl)*sin(theta_ll) - (Fwsl*l_wl + Fbsl*l_bl)*cos(theta_ll)-T_lwl+T_bll;
+eq10 = I_lr*ddtheta_lr == (Fwhr*l_wr + Fbhr*l_br)*sin(theta_lr) - (Fwsr*l_wr + Fbsr*l_br)*cos(theta_lr)-T_lwr+T_blr;
 eq11 = m_b*ddSb - (Fbsl + Fbsr) == 0;
 eq12 = m_b*ddhb - (Fbhl+Fbhr-m_b*g) == 0;
-eq13 = I_b*ddtheta_b - (-(T_bll+T_blr)-(Fbsl+Fbsr)*l_c*cos(theta_b)+(Fbhl+Fbhr)*l_c*sin(theta_b)) == 0;
-eq14 = I_z*ddphi - (-fl+fr)*R_l == 0;
+eq13 = I_b*ddtheta_b == -(T_bll+T_blr)-(Fbsl+Fbsr)*l_c*cos(theta_b)+(Fbhl+Fbhr)*l_c*sin(theta_b);
+eq14 = I_z*ddphi == (-fl+fr)*R_l;
 eq15 = Fwhl-Fwhr == 0;
 
 %%建立替换规则的元胞数组，通过管理rules来实现管理替换变量
@@ -147,26 +147,84 @@ for i = 1:size(rules,1)
     change = subs(change,rules(i,1),rules(i,2));
 end
 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 answer = solve(change,var)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 此处输入机器人的实际参数
+% sym_2_real = {
+%     R_w, 0.06; %轮半径
+%     R_l, 0.15; %机体半径
+%     l_wl, l_l/2; %驱动轮质心到左右腿质心距离
+%     l_wr, l_r/2; %默认为腿长一半
+%     l_bl, l_l/2; %髋关节中点到腿质心距离
+%     l_br, l_r/2; %默认为腿长一半
+%     I_ll, (l_l/2)^2*m_l; %腿的转动惯量，轴心为髋关节中点
+%     I_lr, (l_r/2)^2*m_l;
+%     I_b , l_c^2*m_b;
+%     l_c , 0.005; %身体质心到髋关节中点连线距离
+%     m_w , 0.8; %轮的质量
+%     m_l , 0.168; %腿的质量
+%     m_b , 4.56; %机体质量
+%     I_w , 566.352*1.3/1000000; %轮的转动惯量
+%     I_z , 0.03;
+%     g, 9.8;
+%     };
+%%变量：R_l
+% %下面为调试数据，不要管
 %此处输入机器人的实际参数
 sym_2_real = {
-    R_w, 0.06; %轮半径
-    R_l, 0.15; %机体半径
-    l_wl, l_l/2; %驱动轮质心到左右腿质心距离
-    l_wr, l_r/2; %默认为腿长一半
-    l_bl, l_l/2; %髋关节中点到腿质心距离
-    l_br, l_r/2; %默认为腿长一半
-    l_c , 0.005; %身体质心到髋关节中点连线距离
-    m_w , 0.8; %轮的质量
-    m_l , 0.168; %腿的质量
-    m_b , 4.56; %机体质量
-    I_w , 566.352*1.3/1000/1000; %轮的转动惯量
-    I_ll, (l_l/2)^2*m_l; %腿的转动惯量，轴心为髋关节中点
-    I_lr, (l_r/2)^2*m_l;
-    I_b , l_c^2*m_b;
-    I_z , 0.03;
+    R_w, 1; %轮半径
+    R_l, 1; %机体半径
+    l_wl, 1; %驱动轮质心到左右腿质心距离
+    l_wr, 1; %默认为腿长一半
+    l_bl, 1; %髋关节中点到腿质心距离
+    l_br, 1; %默认为腿长一半
+    I_ll, 1; %腿的转动惯量，轴心为髋关节中点
+    I_lr, 1;
+    I_b , 1;
+    l_c , 1; %身体质心到髋关节中点连线距离
+    m_w , 1; %轮的质量
+    m_l , 1; %腿的质量
+    m_b , 1; %机体质量
+    I_w , 1; %轮的转动惯量
+    I_z , 1;
+    g, 1;
+    l_r, 1;
+    l_l, 1;
     };
-%%变量：R_l
+%变量：R_l
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ddtheta_wl_sol = answer.ddtheta_wl;
+ddtheta_wr_sol = answer.ddtheta_wr;
+ddtheta_ll_sol = answer.ddtheta_ll;
+ddtheta_lr_sol = answer.ddtheta_lr;
+ddtheta_b_sol  = answer.ddtheta_b;
+%%这里是高阶小量，
+
+small = [theta_ll, theta_lr, theta_b];
+
+ddtheta_wl_sol = taylor(ddtheta_wl_sol, [theta_ll, theta_lr, theta_b], 'Order', 2);
+ddtheta_wr_sol = taylor(ddtheta_wr_sol, [theta_ll, theta_lr, theta_b], 'Order', 2);
+ddtheta_ll_sol = taylor(ddtheta_ll_sol, [theta_ll, theta_lr, theta_b], 'Order', 2);
+ddtheta_lr_sol = taylor(ddtheta_lr_sol, [theta_ll, theta_lr, theta_b], 'Order', 2);
+ddtheta_b_sol = taylor(ddtheta_b_sol, [theta_ll, theta_lr, theta_b], 'Order', 2);
+
+
+formulas = [ddtheta_wl_sol,ddtheta_wr_sol,ddtheta_ll_sol,ddtheta_lr_sol,ddtheta_b_sol];
+for i = 1:size(sym_2_real,1)
+    formulas = subs(formulas,sym_2_real(i,1),sym_2_real(i,2));
+end
+for i = 1:length(formulas)
+    disp(formulas(i));
+end
+jacobian_x = [theta_ll,theta_lr,theta_b];
+jacobian_u = [T_lwl,T_lwr,T_bll,T_blr];
+
+J_A_data = jacobian(formulas,jacobian_x)
+J_B_data = jacobian(formulas,jacobian_u)
+
 toc
+
