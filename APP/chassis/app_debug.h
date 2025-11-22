f0874df69e2977948ae211e75acdcc311b906f61
KLnormal <yuyongyikaf530000@163.com>
2025-11-23 00:54:18 +0800

    add fast debug

---
+++ APP/chassis/app_debug.h
@@ -0,0 +1,14 @@
+//
+// Created by 15082 on 2025/11/23.
+//
+
+#ifndef APP_DEBUG_H
+#define APP_DEBUG_H
+
+namespace  AppDebug {
+    typedef enum { FREE_DEBUG, CHASSIS_OUTPUT, INS_RAW_DEG, INS_RAW_ACC, CHASSIS_STATE } Debug_Output;
+inline Debug_Output DEBUG_TYPE = CHASSIS_OUTPUT;
+};
+
+
+#endif //APP_DEBUG_H
